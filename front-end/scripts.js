e0bc39cc984db4b8a154adc9f94af520be44306d
Timmy Phan <timmyp3@uci.edu>
2025-05-10 15:10:36 -0700

    Add menu buttons

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -4,5 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.addEventListener('click', () => {
     alert('Button clicked!');
     });
-    console.log('Page loaded – sidebar width:', document.querySelector('.sidebar').offsetWidth);
+    document.getElementById('menu1').addEventListener('click', menu1Click);
+    document.getElementById('menu2').addEventListener('click', menu2Click);
+    document.getElementById('menu3').addEventListener('click', menu3Click);
 });
+function menu1Click(event) {
+  console.log('Menu 1 clicked');
+  // TODO: put your code here
+}
+
+function menu2Click(event) {
+  console.log('Menu 2 clicked');
+  // TODO: put your code here
+}
+
+function menu3Click(event) {
+  console.log('Menu 3 clicked');
+  // TODO: put your code here
+}
