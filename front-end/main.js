52a45f565cee56624d8e43ac2cdaa42d4c850049
Ken <kyandell@uci.edu>
2025-05-10 22:24:08 -0700

    test3

--- front-end/main.js
+++ front-end/main.js
@@ -51,7 +51,7 @@ function requestGenerateIdea(prompt) {
             'origin': 'https://main.d1zfh5jl8g0um5.amplifyapp.com/',
             'Access-Control-Request-Method': '*'
     },
-    body: data
+    body: JSON.stringify(data)
   };
 
   console.log(data)
