ae3002e0751a88a66f42428f21b6c1c18b566de9
Ken <kyandell@uci.edu>
2025-05-11 12:19:47 -0700

    testing differen tinputs for chat

--- front-end/main.js
+++ front-end/main.js
@@ -118,7 +118,7 @@ function requestGenerateIdea(prompt) {
 }
 
 function chatBoxUse(prompt) {
-    const chatBox = document.getElementById('chatInput')
+    const chatBox = document.getElementById(userMessages)
     const apiUrl = 'https://4rygkzqfcj.execute-api.us-west-2.amazonaws.com/default/claudeChatBox';
     const data = {
         "chatBox": chatBox
